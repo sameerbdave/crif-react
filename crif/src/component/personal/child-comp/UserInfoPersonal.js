a1344da14c447114fb672c004974a6e287f22af5
Sameer Dave <sameerbdave@gmail.com>
2022-10-19 17:26:09 +0530

    personal-form

--- crif/src/component/personal/child-comp/UserInfoPersonal.js
+++ crif/src/component/personal/child-comp/UserInfoPersonal.js
@@ -1,33 +1,61 @@
 import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 
-const UserInfoPersonal = () =>{
-    return(
-        <div className="card-box">
-            <h1 className="hd24">UserInfoPersonal</h1>
-            <form>
+const UserInfoPersonal = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }; // your form submit function which will invoke after successful validation
+
+    console.log(watch("example")); // you can watch individual input by pass the name of the input
+
+    return (
+        <div className="card-box ">
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-group">
-                    <label for="exampleFormControlInput1">Email address</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    <label>First Name</label>
+                    <input
+                        {...register("firstName", {
+                            required: true,
+                            maxLength: 20,
+                            pattern: /^[A-Za-z]+$/i
+                        })}
+                    />
+                    {errors?.firstName?.type === "required" && <p>This field is required</p>}
+                    {errors?.firstName?.type === "maxLength" && (
+                        <p>First name cannot exceed 20 characters</p>
+                    )}
+                    {errors?.firstName?.type === "pattern" && (
+                        <p>Alphabetical characters only</p>
+                    )}
                 </div>
-                <div className="form-group">
-                    <label for="exampleFormControlSelect1">Example select</label>
-                    <select className="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </select>
-                </div>
-               
-                <div className="form-group">
-                    <label for="exampleFormControlTextarea1">Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-                </form>
-        </div>  
-    )
+
+
+
+                <label>Laste Name</label>
+                <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+                {errors?.lastName?.type === "pattern" && (
+                    <p>Alphabetical characters only</p>
+                )}
+                <label>Age</label>a
+                <input {...register("age", { min: 18, max: 99 })} />
+                {errors.age && (
+                    <p>You Must be older then 18 and younger then 99 years old</p>
+                )}
+                <input type="submit" />
+            </form>
+        </div>
+
+    );
 }
 
 export default UserInfoPersonal;
+
